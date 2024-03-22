2ea1ae085ceda1a37a26a24d6d8645b576f34638
Surya Adi Wiguna <142723064+suryaadiwiguna@users.noreply.github.com>
2024-03-22 22:42:57 +0700

    feat: role based sidebar/menubar. fix: menu layout

--- apps/web/src/components/admin/ui/menu-links.ts
+++ apps/web/src/components/admin/ui/menu-links.ts
@@ -1,9 +1,21 @@
+import { IconType } from "react-icons";
 import { PiHouse, PiUser, PiUserGear, PiWarehouse, PiArchive } from "react-icons/pi";
 
-export const links = [
+export type MenuLinks = {
+    name: string;
+    href: string;
+    icon: IconType;
+}[]
+
+export const superAdminLinks = [
     { name: "Home", href: "/admin", icon: PiHouse },
     { name: "Customer", href: "/admin/customer-management", icon: PiUserGear },
     { name: "Warehouse Admin", href: "/admin/admin-management", icon: PiUser },
     { name: "Warehouse", href: "/admin/warehouse-management", icon: PiWarehouse },
+    { name: "Products", href: "/admin/products", icon: PiArchive },
+]
+
+export const warehouseAdminLinks = [
+    { name: "Home", href: "/admin", icon: PiHouse },
     { name: "Products", href: "/admin/products", icon: PiArchive },
 ]
--- apps/web/src/components/admin/ui/sidebar.tsx
+++ apps/web/src/components/admin/ui/sidebar.tsx
@@ -3,15 +3,29 @@
 import clsx from "clsx";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { links } from "@/components/admin/ui/menu-links";
+import { MenuLinks, superAdminLinks, warehouseAdminLinks } from "@/components/admin/ui/menu-links";
+import { useAuth } from "@/lib/store/auth/auth.provider";
+import { useEffect, useState } from "react";
 
 export default function Sidebar() {
     const pathname = usePathname();
+    const auth = useAuth();
+    const role = auth?.user?.data?.role;
+    const [links, setLinks] = useState<MenuLinks | undefined>(undefined);
+
+    useEffect(() => {
+        if (!auth?.isLoading && role === 'WAREHOUSE_ADMIN') {
+            setLinks(warehouseAdminLinks);
+        }
+        if (!auth?.isLoading && role === 'SUPER_ADMIN') {
+            setLinks(superAdminLinks);
+        }
+    }, [role])
 
     return (
         <>
             <div className="flex flex-col space-y-2">
-                {links.map((link) => {
+                {links && links.length && links.map((link) => {
                     const LinkIcon = link.icon;
                     return (
                         <Link
