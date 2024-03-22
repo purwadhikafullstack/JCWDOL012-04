b9ad6abd6a8c109d39c5167adf80ed7293ac1deb
Surya Adi Wiguna <142723064+suryaadiwiguna@users.noreply.github.com>
2024-03-22 14:04:33 +0700

    fix: updated admin sidebar color theme

--- apps/web/src/components/admin/ui/sidebar.tsx
+++ apps/web/src/components/admin/ui/sidebar.tsx
@@ -25,9 +25,9 @@ export default function Sidebar() {
                         <Link
                             key={link.name}
                             href={link.href}
-                            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 hover:text-purple-600 md:flex-none md:justify-start md:p-2 md:px-3",
                                 {
-                                    'bg-sky-100 text-blue-600': pathname === link.href,
+                                    'bg-purple-100 text-purple-600': pathname === link.href,
                                 })}
                         >
                             <LinkIcon />
